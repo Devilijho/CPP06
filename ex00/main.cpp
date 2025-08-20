57fabc9ee9d41dcdb242781858630b6d050891ab
Santiago Fuentes <santi.fuentes.llanos@gmail.com>
2025-08-20 15:56:25 +0200

    bon

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -2,16 +2,8 @@
 
 int	main(int ac, char **argv)
 {
-	(void)argv;
-	(void)ac;
-	// if (ac != 2)
-	// 	return (1);
-	ScalarConverter::convert("0");
-	std::cout << "--------------------------------" << std::endl;
-	ScalarConverter::convert("nan");
-	std::cout << "--------------------------------" << std::endl;
-	ScalarConverter::convert("42.0f");
-
-	// ScalarConverter::convert(argv[1]);
+	if (ac != 2)
+		return (1);
+	ScalarConverter::convert(argv[1]);
 	return (0);
 }
